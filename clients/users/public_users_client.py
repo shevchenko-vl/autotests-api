55426bb76d4c11f6eb5f4e02db1dbda74f221f67
Vladimir Shevchenko <shevchenko-vl@test.org>
2025-08-31 16:39:39 +0300

    Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,31 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import APIClient
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание параметра запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для части "/api/v1/users" запросов без аутентификации.
+    """
+
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        """
+        Создает пользователя.
+
+        :param request: Словарь соответствующий CreateUserRequestDict (note: казалось бы `payload`)
+        :return: Объект Response с данными ответа.
+        """
+        return self.post('/api/v1/users', json=request)
