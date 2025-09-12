6eea3067480f0f3704858e2b29194ec1f11be789
Vladimir Shevchenko <shevchenko-vl@test.org>
2025-09-12 16:29:26 +0300

    Знакомство с Faker

--- httpx_update_user.py
+++ httpx_update_user.py
@@ -1,10 +1,10 @@
 import httpx
 
-from tools.fakers import get_random_email
+from tools.fakers import fake
 
 
 create_user_payload = {
-    'email': get_random_email(),
+    'email': fake.email(),
     'password': 'string',
     'lastName': 'string',
     'firstName': 'string',
@@ -24,7 +24,7 @@ login_response_data = login_response.json()
 
 # Update part:
 update_user_payload = {
-    'email': get_random_email(),
+    'email': fake.email(),
     'lastName': 'string',
     'firstName': 'string',
     'middleName': 'string'
--- pydantic_json_schema_get_user.py
+++ pydantic_json_schema_get_user.py
@@ -2,11 +2,11 @@ from clients.users.private_users_client import get_private_users_client, Authent
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
 from tools.assertions.schema import validate_json_schema
-from tools.fakers import get_random_email
+from tools.fakers import fake
 
 
 create_user_request = CreateUserRequestSchema(
-    email=get_random_email(),
+    email=fake.email(),
     password='string',
     last_name='string',
     first_name='string',
